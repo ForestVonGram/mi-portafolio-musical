b5e6b0c7ab5e93d2b0fd5284ff840655d0bd84ce
DAVID GR <playa200612@gmail.com>
2025-04-22 08:32:59 -0500

    Subida de imágenes para conciertos usando Multipart File.

--- backend/src/main/java/com/davidgomez/miportafoliomusical/config/WebConfig.java
+++ backend/src/main/java/com/davidgomez/miportafoliomusical/config/WebConfig.java
@@ -1,4 +1,15 @@
 package com.davidgomez.miportafoliomusical.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    }
 }
--- backend/src/main/java/com/davidgomez/miportafoliomusical/controller/ConciertoController.java
+++ backend/src/main/java/com/davidgomez/miportafoliomusical/controller/ConciertoController.java
@@ -3,10 +3,18 @@ package com.davidgomez.miportafoliomusical.controller;
 import com.davidgomez.miportafoliomusical.model.Concierto;
 import com.davidgomez.miportafoliomusical.service.ConciertoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/conciertos")
@@ -46,5 +54,22 @@ public class ConciertoController {
     @GetMapping("/proximos")
     public List<Concierto> conciertosProximos() {
         return service.obtenerProximos();
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImagen(@RequestParam("file")MultipartFile file) {
+        try {
+            String folder = System.getProperty("user.dir") + "/uploads/";
+            File directory = new File(folder);
+            if (!directory.exists()) directory.mkdirs();
+
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+            Path path = Paths.get(folder + fileName);
+            Files.write(path, file.getBytes());
+
+            return ResponseEntity.ok("/uploads/" + fileName);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al subir la imagen");
+        }
     }
 }
